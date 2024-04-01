a736020b3ebe5aac0c8b5643965cf500af2db585
Giovanni <giovannimaerean@gmail.com>
2024-04-02 00:03:59 +0200

    add scatter commit

---
+++ generateScatterDiagram.py
@@ -0,0 +1,38 @@
+import os
+from github import Github
+import matplotlib.pyplot as plt
+
+token = os.getenv('GITHUB_TOKEN')
+repo_name = "username/repo_name"
+
+g = Github(token)
+repo = g.get_repo(repo_name)
+
+
+def main() -> None:
+    issues = repo.get_issues()
+    priorities = []
+    estimations = []
+
+    for issue in issues:
+
+        if 'priority' in issue.raw_data['fields']:
+            priority = issue.raw_data['fields']['priority']
+            priorities.append(priority)
+        if 'estimate' in issue.raw_data['fields']:
+            estimate = issue.raw_data['fields']['estimate']
+            estimations.append(estimate)
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(estimations, priorities, color='blue', alpha=0.5)
+
+    plt.xlabel('Estimation')
+    plt.ylabel('Priority')
+    plt.title('Scatter Diagram of Estimations and Priorities')
+    plt.grid(True)
+
+    plt.savefig("scatter_diagram_issues.png")
+
+
+if __name__ == '__main__':
+    main()

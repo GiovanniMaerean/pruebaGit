d5cc7c148d044f96996b0f32cd86ca783b926959
Giovanni <114426285+GiovanniMaerean@users.noreply.github.com>
2024-04-04 17:01:58 +0200

    Update generateScatterDiagram.py

--- generateScatterDiagram.py
+++ generateScatterDiagram.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 token = os.getenv('GITHUB_TOKEN')
 
-repo_name = "arnaums02/Joint-Project---Grup-B"
+repo_name = "GiovanniMaerean/pruebaGit"
 
 g = Github(token)
 repo = g.get_repo(repo_name)

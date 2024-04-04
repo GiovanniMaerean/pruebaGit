7f854a5622498295e80ded830eb6a8b22b5ec984
Giovanni <114426285+GiovanniMaerean@users.noreply.github.com>
2024-04-04 17:13:39 +0200

    Update generateWeeklyScatterDiagram.py

--- generateWeeklyScatterDiagram.py
+++ generateWeeklyScatterDiagram.py
@@ -2,27 +2,28 @@ import os
 from datetime import datetime, timezone, timedelta
 from github import Github
 import matplotlib.pyplot as plt
-from generateScatterDiagram import token, repo_name
-# This code generates a scatter diagram with all the commits
-# done on this week, showing the added lines and the modified
-# files. It runs automatically every Sunday on 23:59 using the
-# "scatterWeeklyGenerator.yml" workflow.
 
+# Token de autenticación de GitHub
+token = os.getenv('GITHUB_TOKEN')
+
+# Nombre del repositorio en formato "username/repository"
+repo_name = "GiovanniMaerean/pruebaGit"
+
+# Inicialización de la instancia de Github
 g = Github(token)
+
+# Obtener el repositorio
 repo = g.get_repo(repo_name)
 
 days_to_close = []
 issue_created_dates = []
 
-
-
 def main() -> None:
     get_data()
     generate_diagram()
 
-
 def get_date_range_of_current_week() -> tuple:
-    """Gets the date range of the current week from Monday to Sunday as datetime objects, making them timezone aware."""
+    """Obtener el rango de fechas de la semana actual desde el lunes hasta el domingo como objetos datetime, haciéndolos conscientes de la zona horaria."""
     current_date = datetime.now(timezone.utc)
     start_of_week = current_date - timedelta(days=current_date.weekday())
     end_of_week = start_of_week + timedelta(days=6)
@@ -30,11 +31,10 @@ def get_date_range_of_current_week() -> tuple:
     end_of_week = datetime.combine(end_of_week.date(), datetime.max.time(), tzinfo=timezone.utc)
     return start_of_week, end_of_week
 
-
 def get_data() -> None:
-    """Gets all the closed issues with their open date and time to close"""
+    """Obtener los datos de las issues cerradas durante esta semana"""
     start_of_week, end_of_week = get_date_range_of_current_week()
-    issues = repo.get_issues(since=start_of_week)
+    issues = repo.get_issues(state='closed', since=start_of_week, until=end_of_week)
 
     for issue in issues:
         created_at = issue.created_at
@@ -45,9 +45,8 @@ def get_data() -> None:
             days_to_close.append(time_to_close)
             issue_created_dates.append(created_at)
 
-
 def generate_diagram() -> None:
-    """Generates and saves the scatter diagram with all the data"""
+    """Generar y guardar el scatter diagram con todos los datos"""
     plt.figure(figsize=(10, 6))
 
     plt.scatter(days_to_close, issue_created_dates, color='blue', alpha=0.7)
@@ -55,14 +54,13 @@ def generate_diagram() -> None:
     plt.xlabel('Días para cerrar la issue')
     plt.ylabel('Fecha de creación de la issue')
     plt.title(f'Scatter Diagram of closed issues - {get_date()}')
-    plt.legend()
     plt.grid(True)
 
     plt.savefig("scatter_diagram_issues_weekly.png")
-
+    plt.show()
 
 def get_date() -> str:
-    """The format is: MX-WY-Report -> where X is the month number and Y is the week number"""
+    """El formato es: MX-WY-Report -> donde X es el número de mes y Y es el número de semana"""
     now = datetime.now()
     month = now.strftime("%m")
     week_of_year = int(now.strftime("%U"))
@@ -70,7 +68,6 @@ def get_date() -> str:
     week_of_first_day = int(first_day_of_month.strftime("%U"))
     week_of_month = week_of_year - week_of_first_day + 1
     return f"M{month}-W{week_of_month}-Report"
-
 
 if __name__ == '__main__':
     main()
